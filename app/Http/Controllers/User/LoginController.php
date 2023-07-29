30e5b37b0567eb5be0391b0482fef221f47a3cec
Mr-Monty999 <montser.mohmed2022@gmail.com>
2023-07-29 12:53:31 +0300

    make login only for active users

--- app/Http/Controllers/User/LoginController.php
+++ app/Http/Controllers/User/LoginController.php
@@ -15,8 +15,9 @@ class LoginController extends Controller
     }
     public function login(Request $request)
     {
-        // return $request->all();
-        if (Auth::attempt($request->only("email", "password"))) {
+        $request->merge(["is_active" => true]);
+
+        if (Auth::attempt($request->only("email", "password", "is_active"))) {
             toastr()->success(trans('keywords.Logged In Successfully'));
             return redirect()->route("user.home");
         }
