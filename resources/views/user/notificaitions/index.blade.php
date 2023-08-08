cff8f7168bf860fc576ae1046ebf19270a17b1e0
Mr-Monty999 <montser.mohmed2022@gmail.com>
2023-08-08 16:01:31 +0300

    add created at on notifications

--- resources/views/user/notificaitions/index.blade.php
+++ resources/views/user/notificaitions/index.blade.php
@@ -14,15 +14,17 @@
             @foreach ($notifications as $index => $notification)
                 @if ($notification->type == 'App\Notifications\LandOfferAcceptedNotification')
                     <a style="width: 100%; margin-bottom:1px;padding:20px;border-radius: 10px;" class="alert-info text-center"
+                        href="{{ route('user.land-offers.show', $notification->data['land_offer_id']) }}">
                         @php
-$user = User::find($notification->data['user_id']); @endphp
-                        href="{{ route('user.land-offers.show', $notification->data['land_offer_id']) }}">{{ trans('keywords.user') . ' ' . $user->name . ' ' . trans('keywords.has accepted your offer') }}</a>
+                            $user = User::find($notification->data['user_id']);
+                        $landOffer = \App\Models\LandOffer::find($notification->data['land_offer_id']); @endphp
+                        {{ trans('keywords.user') . ' ' . $user->name . ' ' . trans('keywords.has accepted your offer') }}
+                        <div class="text-black">
+                            {{ $landOffer->created_at->diffForHumans() }}
+                        </div>
+                    </a>
                 @endif
-                {{-- @if ($index != count($notifications) - 1)
-                    <div class="dropdown-divider"></div>
-                @endif --}}
             @endforeach
-
         </div>
         {!! $notifications->links() !!}
     </div>
